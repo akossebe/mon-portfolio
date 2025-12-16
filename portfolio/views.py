ead0c6b99356a0389804824ef6b180ff3963bb3f
weezy <kolomsoakossebe@gmail.com>
2025-12-16 11:08:55 +0100

    test d'affichage et reglages des urls

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        "nom": "Mouktar",
+        "universite": "Université de Yaoundé I",
+        "filiere": "Technologie de l’Information et de la Communication (ICT)",
+        "message": "Bienvenue sur mon premier portfolio Django"
+    }
+    return render(request, 'portfolio/index.html', context)
